96af5f0ad704473eddf2d24949199af5f1b53971
joão <joao.nicolete@usp.br>
2021-07-07 12:14:32 -0300

    adicionando get by id

--- src/main/java/br/org/habilidades/blogpessoal/controller/UsuarioController.java
+++ src/main/java/br/org/habilidades/blogpessoal/controller/UsuarioController.java
@@ -8,12 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.habilidades.blogpessoal.model.UserLogin;
 import br.org.habilidades.blogpessoal.model.Usuario;
 import br.org.habilidades.blogpessoal.service.UsuarioService;
+import br.org.habilidades.blogpessoal.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -22,6 +25,9 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
@@ -33,6 +39,12 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());				
 	}
 	
 }
